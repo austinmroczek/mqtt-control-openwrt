8459bde2d275d323788132aba23aad255d804c5d
Austin Mroczek <austin@mroczek.org>
2021-12-05 10:38:13 -0800

    make sub generic

--- mosquitto_sub_ipad.sh
+++ mosquitto_sub_ipad.sh
@@ -2,19 +2,19 @@
 host="$1"
 device="$2"
 mac="$3"
-p="mqttPipe$device"
+p="mqttPipe_$device"
 
 
 ([ ! -p "$p" ]) && mkfifo $p
-(mosquitto_sub -h $host -q 1 -t ha/internet/ipad >$p 2>/dev/null) &
+(mosquitto_sub -h $host -q 1 -t ha/internet/$device >$p 2>/dev/null) &
 PID=$!
 
 trap 'kill $PID' HUP INT TERM QUIT KILL
 
 while read line <$p
 do
-   echo $line > /tmp/ipad
-      TASK=`cat /tmp/ipad`
+   echo $line > /tmp/$device
+      TASK=`cat /tmp/$device`
       if [ "$TASK" == "OFF" ]; then
          /etc/iot/disable.sh $device $mac
       elif [ "$TASK" == "ON" ]; then
